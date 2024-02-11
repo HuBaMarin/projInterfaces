be6528a00225cff826ebe133cdac82575879d8ed
HuBaMarin <marinlosenlaces@gmail.com>
2024-02-11 13:53:57 +0100

    cambios

--- index.js
+++ index.js
@@ -17,31 +17,57 @@ function comprobarUsuario() {
 function updateUI() {
 
     if (comprobarUsuario()) {
-        //recoger login de localStorage y esconder los elementos register y login
-        let nombre = JSON.parse(localStorage.getItem('usuario')).nombre;
-        const loginButton = document.querySelector('a[href="iniciarSesion.html"]');
-        const registerButton = document.querySelector('a[href="registrarse.html"]');
 
-        loginButton.style.display = 'none';
-        registerButton.style.display = 'none';
+        //si vengo de la página de iniciarSesion, esconder el boton de iniciarSesion
+        if (window.location.href.includes('iniciarSesion.html')) {
+
+            //recoger login de localStorage y esconder los elementos register y login
+            let nombre = JSON.parse(localStorage.getItem('usuario')).nombre;
+            const loginButton = document.querySelector('a[href="iniciarSesion.html"]');
+            const registerButton = document.querySelector('a[href="registrarse.html"]');
+
+            loginButton.style.display = 'none';
+            registerButton.style.display = 'none';
+
+            //recoger un texto de index y poner el nombre
+            let texto = document.querySelector("h1");
+
+            texto.textContent = "¡Bienvenido/a a la Tienda de Bicicletas " + nombre + " ! 🚴‍♂️🚴‍♀️";
+            // mostrar una imagen y poner la funcion de cerrar sesión debajo
+
+            //recoger un elemento del index del nav y poner la imagen
+            let ponerImagen =
+                `<div>
+                  <img src="bruhhh.jpg" class="dropbtn"  alt="imagen"/>
+                  <li id="desplegable">
+                    <a href="#">Cerrar Sesión</a>
+                  </li>
+                </div>`;
 
 
-        //recoger un texto de index y poner el nombre
-        let texto = document.querySelector("h1");
-
-        texto.textContent = "¡Bienvenido/a a la Tienda de Bicicletas " +nombre+" ! 🚴‍♂️🚴‍♀️";
-        // mostrar una imagen y poner la funcion de cerrar sesión debajo
+            let desplegable = document.getElementById("desplegable");
+            desplegable.style.display = "block";
 
 
-        //recoger un elemento del index y poner la imagen
-        let ponerImagen =
-            `<div class="dropdown">
-          <img src="bruhhh.jpg" class="dropbtn" onclick="toggleDropdown()"  alt="imagen"/>
-          <div id="myDropdown">
-            <a href="#">Cerrar Sesión</a>
-          </div>
-        </div>`;
+            desplegable.addEventListener("click", function () {
+                desplegable.style.display = "none";
+                loginButton.style.display = 'block';
+                registerButton.style.display = 'block';
+                localStorage.removeItem('usuario');
+                location.reload();
+            });
 
+
+
+            let nav = document.querySelector(".indexNav");
+
+            nav.innerHTML += ponerImagen;
+        }
+
+        //si vengo de la pagina de registrarse, esconder el boton de registrarse
+        if (window.location.href.includes('registrarse.html')) {
+            registerButton.style.display = 'none';
+        }
 
     }
 }
--- iniciarSesion.js
+++ iniciarSesion.js
@@ -1,10 +1,31 @@
 /*comprobar la contraseña y el usuario al iniciar sesion*/
-function comprobarContrasenia(username, password) {
-    const users = JSON.parse(localStorage.getItem('user'));
-    for (let i = 0; i < users.length; i++) {
-        if (users[i].username === username && users[i].password === password) {
-            return true;
+function comprobarContrasenia() {
+    const users = JSON.parse(localStorage.getItem('usuario'));
+
+    document.getElementById("iniciarSesion").addEventListener("submit", (e) => {
+        e.preventDefault();
+        const nom = document.querySelector("#nombre").value;
+        const pass = document.querySelector("#password").value;
+
+
+
+        //comprobar usuario y contraseña
+        if (users.nombre !== nom && users.password !== pass) {
+            document.querySelector(".error1").innerHTML = "usuario incorrecto";
+
+            document.querySelector(".error2").innerHTML = "contraseña incorrecta";
         }
-    }
-    return false;
+
+        if (users.nombre !== nom) {
+            document.querySelector(".error1").innerHTML = "usuario incorrecto";
+        } else if (users.password !== pass) {
+            document.querySelector(".error2").innerHTML = "contraseña incorrecta";
+        } else {
+            document.querySelector(".error1").innerHTML = "";
+            document.querySelector(".error2").innerHTML = "";
+
+            window.location.href = "index.html";
+        }
+    });
+
 }
